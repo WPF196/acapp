cd35eaccc0810d247cf21ffbc040da0975a49665
WPF196 <1962978635@qq.com>
2023-03-05 12:04:36 +0800

    write my first page

--- acapp/urls.py
+++ acapp/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('game.urls')),     #当接收为空，调用game.urls路由
+    #如path('game', include('a.urls')   网页/game 时，调用a.urls
     path('admin/', admin.site.urls),
 ]
--- game/views.py
+++ game/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    line1 = '<h1 style = "text-align: center">术士之战</h1>'
+    line4 = '<a href = "/play/">进入游戏界面</a>'
+    line3 = '<hr>'
+    line2 = '<img src = "https://img0.baidu.com/it/u=2857096089,2095994689&fm=253&fmt=auto&app=138&f=JPEG?w=1070&h=500" width = 800>'
+    return HttpResponse(line1 + line4 + line3 + line2)
+
+def play(request):
+    line1 = '<h1 style = "text-align: center">游戏界面</h1>'
+    line4 = '<a href = "/">返回主页面</a>'
+    line3 = '<hr>'
+    line2 = '<img src = "https://img0.baidu.com/it/u=4115416391,3766188720&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500" width = 800>'
+    return HttpResponse(line1 + line4 + line3 + line2)
