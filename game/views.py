90227ddcc8939d7e2af1fad92b09e0a0b3ba7d4d
WPF196 <1962978635@qq.com>
2023-03-11 21:10:02 +0800

    write my second page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line4 = '<a href="/play/">进入游戏界面</a>'
+    line3 = '<hr>'
+    line2 = '<img src="https://th.bing.com/th/id/OIP.i-meFDtJMvq33tiFKZgkTAHaIR?w=196&h=219&c=7&r=0&o=5&dpr=1.5&pid=1.7" width=200>'
+    return HttpResponse(line1 + line4 + line3 + line2)
+
+
+def play(request):
+    line1 = '<h1 style="text-align: center">游戏界面</h1>'
+    line3 = '<a href="/">返回主页面</a>'
+    line2 = '<img src="https://th.bing.com/th/id/OIP.vaM2ge-wUDY7Us01z0EcwgHaIW?w=194&h=219&c=7&r=0&o=5&dpr=1.5&pid=1.7" width=200>'
+    return HttpResponse(line1 + line3 + line2)
+
